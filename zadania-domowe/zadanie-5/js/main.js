94a5bb279938684ff325d2ef0027af70127546e8
Marcin Biedal <marcinbiedal@wp.pl>
2017-11-04 21:38:03 +0100

    update

--- zadania-domowe/zadanie-5/js/main.js
+++ zadania-domowe/zadanie-5/js/main.js
@@ -6,8 +6,14 @@ $(document).ready(function () {
 
 function downloadJson() {
     $('.download').click(function () {
-        $.post("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function (data) {
-            $('.dane-programisty').html(data);
+        $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function (data) {
+            if ($('#dane-programisty').length < 1) {
+                $('body').append('<div></div>');
+                $('div').attr('id', 'dane-programisty');
+            } 
+            $.each(data, function (index, element) {
+                $('#dane-programisty').append('<p>' + index + ': ' + element + '<p>'); 
+            }); 
         });
     });
 }
