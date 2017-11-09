cf3af9f269a0903eacf8c4c102069ea2c17de84b
Marcin Biedal <marcinbiedal@wp.pl>
2017-11-09 16:54:49 +0100

    Resize slaydera

--- portfolio2/app/js/main.js
+++ portfolio2/app/js/main.js
@@ -67,7 +67,9 @@ function autoShow() {
     for (i = 0; i < x.length; i++) {
         x[i].style.display = 'none';
     }
-    if(index > x.length){index = 1}
+    if (index > x.length) {
+        index = 1
+    }
 
     x[index - 1].style.display = 'block';
     index++;
