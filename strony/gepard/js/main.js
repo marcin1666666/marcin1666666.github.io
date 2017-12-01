c34efd340b1fe3b76c73c88700da52ec359775d2
Marcin Biedal <marcinbiedal@wp.pl>
2017-12-01 12:30:10 +0100

    Ready

--- strony/gepard/js/main.js
+++ strony/gepard/js/main.js
@@ -1,1 +1,35 @@
 baguetteBox.run('.tz-gallery');
+$(document).ready(function () {
+    addScrolled();
+    smooth();
+});
+
+$(document).scroll(function () {
+    addScrolled();
+});
+
+function addScrolled() {
+    var height = $('#main-nav').height();
+    var scroll = $(window).scrollTop();
+
+    if (scroll > height) {
+        $('#main-nav').addClass('transparent-background');
+    } else {
+        $('#main-nav').removeClass('transparent-background');
+    }
+}
+
+function smooth() {
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').animate({
+            'scrollTop': $target.offset().top
+        }, 2000, 'swing');
+
+    });
+}
+
