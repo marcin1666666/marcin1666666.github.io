7fa734592badf953d87f3342583bbb6fdf1ecd94
Marcin Biedal <marcinbiedal@wp.pl>
2018-10-22 22:59:08 +0100

    RWD + Update

--- new_my_page/js/main.js
+++ new_my_page/js/main.js
@@ -1,3 +1,24 @@
+$(document).ready(function () {
+    smooth();
+});
+
+function smooth() {
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').animate({
+            'scrollTop': $target.offset().top
+        }, 2000, 'swing');
+
+    });
+}
+
+
+//menu function
+
 function myFunction() {
     document.getElementById("menu-icon").classList.toggle("change");
 }
