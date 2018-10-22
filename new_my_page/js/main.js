44909f90949b832fb4ebc68498e08d2879893f7c
Marcin Biedal <marcinbiedal@wp.pl>
2018-10-22 23:18:58 +0100

    no message

---
+++ new_my_page/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    smooth();
+});
+
+function smooth() {
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').animate({
+            'scrollTop': $target.offset().top
+        }, 2000, 'swing');
+
+    });
+}
+
+
+//menu function//
+
+function myFunction() {
+    document.getElementById("menu-icon").classList.toggle("change");
+}
+
+function display_menu() {
+    document.getElementById("hide").classList.toggle("visible");
+}
