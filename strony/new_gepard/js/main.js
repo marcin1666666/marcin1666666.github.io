ec345549dff035b34080f2ab8662b7e386b89ec3
Marcin Biedal <marcinbiedal@wp.pl>
2019-03-03 19:35:00 +0000

    adding first stage of RWD plus update js file

--- strony/new_gepard/js/main.js
+++ strony/new_gepard/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     scroll();
+    smooth();
 });
 
 $(window).scroll(function(scroll){
@@ -11,3 +12,28 @@ $(window).scroll(function(scroll){
         $("header").removeClass('scrolled');
     }
 });
+
+
+function smooth() {
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').animate({
+            'scrollTop': $target.offset().top
+        }, 1000, 'swing');
+
+    });
+}
+
+//menu function//
+
+function crossX() {
+    document.getElementById("menu-icon").classList.toggle("change");
+}
+
+function display_menu() {
+    document.getElementById("hide").classList.toggle("visible");
+}
